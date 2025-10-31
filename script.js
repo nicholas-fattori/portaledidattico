3ed6aa80a765583ebce374195ae8218446d03d5f
nicholas-fattori <nicholasfattori@gmail.com>
2025-10-31 09:25:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function mostra_mail() {
+    document.write("mail: <a href=\"mailto:viis01100n@istruzione.it\">viis01100n@istruzione.it</a> - pec: <a href=\"mailto:viis01100n@pec.istruzione.it\">viis01100n@pec.istruzione.it</a>");
+}
