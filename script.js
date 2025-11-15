d60d126fed4e2cb2a759c48e9569ad2dd43d115b
nicholas-fattori <nicholasfattori@gmail.com>
2025-11-15 20:16:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function mostra_mail() {
-    document.write("Per contattare il docente: <a href=\"mailto:nicholas.fattori01@iislonigo.onmicrosoft.com\">nicholas.fattori01@iislonigo.onmicrosoft.com</a>");
+    document.write("Per contattare il docente: <a href=\"mailto:nicholas.fattori@iislonigo.onmicrosoft.com\">nicholas.fattori@iislonigo.onmicrosoft.com</a>");
 
 }
+
